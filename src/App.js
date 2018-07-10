2a99daf7780faf9dd70ec868e73f37dcaaecb50b
marjan <Marjan_maruchak@icloud.com>
2018-07-10 17:30:26 +0300

    fix hide menu

--- src/App.js
+++ src/App.js
@@ -9,23 +9,41 @@ class App extends Component {
         this.state = {
             random: null,
             timer: null,
+            min: 1,
+            max: 100,
+            hidden:false,
+            configBoostrap: 'col-md-9 view',
         }
+        this.random = this.random.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        console.log(this.state);
     }
 
     //timer
-    componentDidMount() {
+    componentWillMount(){
         let timer = setInterval(this.random, 2500);
         this.setState({timer});
+
+    }
+
+    handleBtnClick(){
+        this.setState({ hidden: !this.state.hidden});
+        if (this.state.hidden === true ) {
+            this.setState({configBoostrap: 'col-md-9 view'});
+        }
+        else {
+            this.setState({configBoostrap: 'col-md-12 view'});
+            console.log(this.state.configBoostrap);
+        }
     }
 
     buttonStop(data) {
         clearInterval(data);
     }
-    min = 1;
-    max = 100;
 
-    random = () => {
-        this.setState({random: this.min + (Math.random() * (this.max - this.min))});
+    random (){
+        console.log(this.state);
+        this.setState({random: this.state.min + (Math.random() * ( this.state.max -  this.state.min))});
     };
 
     render() {
@@ -34,21 +52,21 @@ class App extends Component {
 
             <div className="App">
                 <header className="App-header">
-                    <button onClick="">hide</button>
+                    <button className="hideCollumButton" onClick={this.handleBtnClick.bind()}>hide</button>
                     <h1 className="App-title">Random</h1>
                 </header>
 
                 <div className="conteiner">
                     <div className="row clas" >
 
-                        <div className="col-md-3 menu">
+                        <div className="col-md-3 menu" hidden={this.state.hidden}>
                             <div className="row"> <h1>Menu</h1></div>
                         </div>
-                        <div className="col-md-9 view">
+                        <div className={this.state.configBoostrap} >
                             <h1>Number {this.state.random}</h1>
                             <p className="App-intro">
                                 <button onClick={this.random.bind()}>Force Rand</button>
-                                <button onClick={this.buttonStop.bind(this,this.state.timer)}>Стоп</button>
+                                <button onClick={this.buttonStop.bind(this,this.state.timer)}>Stop</button>
                             </p>
                         </div>
                     </div>
